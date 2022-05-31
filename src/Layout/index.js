255dd4c1ba2a327032bfa4873e5f2541a396326f
Kate <neilsenk09@gmail.com>
2022-05-30 23:12:14 -0400

    added route to deck and study

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -3,6 +3,8 @@ import Header from "./Header";
 import NotFound from "./NotFound";
 import Home from "./Home";
 import CreateDeckForm from "./Deck/CreateDeckForm";
+import Deck from "./Deck/Deck";
+import StudyDeck from "./Study/StudyDeck";
 import { Route, Switch } from "react-router-dom";
 
 function Layout() {
@@ -18,6 +20,14 @@ function Layout() {
 
           <Route exact path="/decks/new">
             <CreateDeckForm />
+          </Route>
+
+          <Route path="/decks/:deckId">
+            <Deck />
+          </Route>
+
+          <Route path="/decks/:deckId/study">
+            <StudyDeck />
           </Route>
 
           <Route>
