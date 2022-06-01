f53532b55577097a6ceb1be4534a60466c07aee7
Kate <neilsenk09@gmail.com>
2022-05-31 23:23:45 -0400

    updated to included form and state

--- src/Layout/Card/AddCard.js
+++ src/Layout/Card/AddCard.js
@@ -1,5 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-function AddCard() {}
+export default function AddCard({ deck }) {
+  const initialFormState = {
+    front: "",
+    back: "",
+  };
 
-export default AddCard;
+  const [cardData, setCardData] = useState({ ...initialFormState });
+  const handleChange = ({ target }) => {
+    setCardData({
+      ...cardData,
+      [target.name]: target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submitted:", cardData);
+    setCardData({ ...initialFormState });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="front">
+        Front
+        <textarea
+          type="text"
+          name="front"
+          onChange={handleChange}
+          value={cardData.front}
+        />
+      </label>
+      <label htmlFor="back">
+        Back
+        <textarea
+          type="text"
+          name="back"
+          onChange={handleChange}
+          value={cardData.back}
+        />
+      </label>
+      <button className="btn btn-sm btn-secondary">Done</button>
+      <button className="btn btn-sm btn-primary">Save</button>
+    </form>
+  );
+}
