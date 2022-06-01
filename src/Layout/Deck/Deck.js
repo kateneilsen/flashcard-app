79c5590d96118dd0146d6ed4d461c3731511cf87
Kate <neilsenk09@gmail.com>
2022-05-31 23:24:42 -0400

    added useEffect and card styling

--- src/Layout/Deck/Deck.js
+++ src/Layout/Deck/Deck.js
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { readDeck } from "../../utils/api";
 
-function Deck() {}
+function Deck({ deck, setDeck }) {
+  useEffect(() => {
+    const abortController = new AbortController();
+    async function loadDeck() {
+      try {
+        const response = await readDeck(deck.id, abortController.signal);
+        setDeck(response);
+      } catch (error) {
+        console.log(error);
+      }
+
+      return () => {
+        abortController.abort();
+      };
+    }
+    loadDeck();
+  }, []);
+
+  return (
+    <div key={deck.id} className="card">
+      <div className="card-body">
+        <h5 className="card-title">{deck.name}</h5>
+        <p className="card-text">{deck.description}</p>
+        <button className="btn btn-sm btn-secondary">Edit</button>
+        <button className="btn btn-sm btn-primary">Study</button>
+        <button className="btn btn-sm btn-primary">Add Cards</button>
+      </div>
+    </div>
+  );
+}
 
 export default Deck;
