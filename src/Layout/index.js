9a86e3d36cf88f3ee0ee08eb0ef07bdf39bed91a
Kate <neilsenk09@gmail.com>
2022-06-04 13:36:51 -0400

    added decks useState and useEffect

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { listDecks } from "../utils/api";
 import Header from "./Header";
 import NotFound from "./NotFound";
 import Home from "./Home";
@@ -6,9 +7,30 @@ import CreateDeckForm from "./Deck/CreateDeckForm";
 import Deck from "./Deck/Deck";
 import StudyDeck from "./Study/StudyDeck";
 import AddCard from "./Card/AddCard";
+import EditDeck from "./Deck/EditDeck";
 import { Route, Switch } from "react-router-dom";
 
 function Layout() {
+  const [decks, setDecks] = useState([]);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    async function loadDecks() {
+      try {
+        const response = await listDecks(abortController.signal);
+        console.log("setDecks:", response);
+        setDecks(response);
+      } catch (error) {
+        console.log(error);
+      }
+
+      return () => {
+        abortController.abort();
+      };
+    }
+    loadDecks();
+  }, []);
+
   return (
     <>
       <Header />
@@ -16,7 +38,7 @@ function Layout() {
         {/* TODO: Implement the screen starting here */}
         <Switch>
           <Route exact path="/">
-            <Home />
+            <Home decks={decks} />
           </Route>
 
           <Route exact path="/decks/new">
@@ -27,8 +49,12 @@ function Layout() {
             <AddCard />
           </Route>
 
+          <Route path="/decks/:deckId/edit">
+            <EditDeck />
+          </Route>
+
           <Route path="/decks/:deckId/study">
-            <StudyDeck />
+            <StudyDeck decks={decks} />
           </Route>
 
           <Route path="/decks/:deckId">
