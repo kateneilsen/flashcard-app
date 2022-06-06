f83683d2a708745744635155ef3c69d1e6a5e1c9
Kate <neilsenk09@gmail.com>
2022-06-06 15:48:43 -0400

    useEffect added to StudyDeck

--- src/Layout/Study/Flashcard.js
+++ src/Layout/Study/Flashcard.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Flashcard() {
-  return <div>Flashcard</div>;
+export default function Flashcard({ cards, card, index }) {
+  const [flipped, setFlipped] = useState(false);
+  const [cardIndex, setCardIndex] = useState(0);
+
+  const handleCardIndex = () => {
+    const next = cardIndex + 1 === cards.length ? 0 : cardIndex + 1;
+    console.log(next);
+    setCardIndex(next);
+  };
+
+  return (
+    <div>
+      <div className="card">
+        <div className="card-body">
+          <p>flashcard front</p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/Layout/Study/StudyDeck.js
+++ src/Layout/Study/StudyDeck.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import StudyNav from "./StudyNav";
+import Flashcard from "./Flashcard";
+import { readDeck } from "../../utils/api";
 
-export default function StudyDeck({ decks }) {
+export default function StudyDeck() {
   const { deckId } = useParams();
-  const deck = decks.find((deck) => deck.id == deckId);
+  const [deck, setDeck] = useState([]);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    async function loadDeck() {
+      try {
+        const response = await readDeck(deckId, abortController.signal);
+        console.log(response);
+        setDeck(response);
+      } catch (error) {
+        console.log(error);
+      }
+
+      return () => {
+        abortController.abort();
+      };
+    }
+    loadDeck();
+  }, [deckId]);
+
   console.log(deck);
   console.log(deckId);
+  const cards = deck.cards;
+  console.log(cards);
+
   return (
     <div>
       <StudyNav deck={deck} />
-
       <h1>Study: {deck?.name}</h1>
+      <Flashcard />
     </div>
   );
 }
