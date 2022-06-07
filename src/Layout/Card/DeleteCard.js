3aca5b4d43c18bd74907ab9303416ab3ad011938
Kate <neilsenk09@gmail.com>
2022-06-06 22:03:14 -0400

    created delete Card comp and prompt

--- src/Layout/Card/DeleteCard.js
+++ src/Layout/Card/DeleteCard.js
@@ -1,5 +1,28 @@
 import React from "react";
+import { deleteCard } from "../../utils/api";
+import { useHistory } from "react-router-dom";
 
-function DeleteCard() {}
+function DeleteCard() {
+  const history = useHistory();
+
+  const handleDeleteCard = () => {
+    const abortController = new AbortController();
+    deleteCard(cardId, abortController.signal).then(() => history.push("/"));
+  };
+  return (
+    <button
+      className="btn btn-danger"
+      onClick={() =>
+        window.confirm(
+          "Delete this deck? \n\n You will not be able to recover it."
+        )
+          ? deleteFunction()
+          : console.log("You clicked cancel")
+      }
+    >
+      DeleteDeck
+    </button>
+  );
+}
 
 export default DeleteCard;
